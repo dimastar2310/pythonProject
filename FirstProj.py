7c6c072811eeac516865d822667a79ab90093352
dim4akra2310 <34384550+dimastar2310@users.noreply.github.com>
2021-11-03 20:26:51 +0200

    Initial commit

---
+++ FirstProj.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    x = 5
+    print(x)
+    my_list= [1 ,"apples",'2',1.2333]
+    print(my_list)
+    x = 5**3
+    print(x)
+    for i in range(1,len(my_list)):
+        print(my_list[i])
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
